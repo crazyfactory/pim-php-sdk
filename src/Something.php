ee053827beb3f536dba2cee5d7b77bf75cc2cb18
Wolf Mathes <Wolf Mathes>
2016-03-10 11:29:24 +0700

    Initial commit: composer boilerplate

---
+++ src/Something.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PIM\API;
+
+/**
+ * Something
+ *
+ */
+class Something
+{
+    public function __construct()
+    {
+        //
+    }
+}
+
---
+++ src/models/User.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wolf
+ * Date: 3/10/2016
+ * Time: 11:21
+ */
+
+namespace PIM\Models;
+
+
+class User
+{
+	public function __construct()
+	{
+		//
+	}
+}
